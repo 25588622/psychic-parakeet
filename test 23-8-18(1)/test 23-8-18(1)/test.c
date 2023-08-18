f7fdbdd0bec8096d2012eed398ad26b054a1789c
psychic-parakeet <25588622@qq.com>
2023-08-18 22:52:36 +0800

    求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和，其中a是一个数字，（例如：2 + 22 + 222 + 2222 + 22222）

---
+++ test 23-8-18(1)/test 23-8-18(1)/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和，其中a是一个数字，（例如：2 + 22 + 222 + 2222 + 22222）
+
+int main()
+{
+	int a = 0;
+	int n = 0;
+	scanf("%d %d", &a, &n);
+	int sum = 0;
+	int ret = 0;
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		ret = ret * 10 + a;
+		sum += ret;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
