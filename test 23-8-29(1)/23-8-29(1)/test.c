8764abe290baa32f644148abc5aa5cf89d753f91
psychic-parakeet <25588622@qq.com>
2023-08-29 21:13:53 +0800

    调整奇数偶数顺序（将数组中所有的奇数位于数组的前半部分，所有偶数位于数组的后半部分）

---
+++ test 23-8-29(1)/23-8-29(1)/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//调整奇数偶数顺序（将数组中所有的奇数位于数组的前半部分，所有偶数位于数组的后半部分）
+
+
+void move(int* arr, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		while ((left < right) && (arr[left] % 2 == 1))
+		{
+			left++;
+		}
+		while ((left < right) && (arr[right] % 2 == 0))
+		{
+			right--;
+		}
+		if (left < right)
+		{
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+		}
+	}
+}
+void print(int* arr, int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+int main(void)
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	move(arr, sz);
+	print(arr, sz);
+	return 0;
+}
